bb444e880ac34e7afb2f34ef3bbe702a84774642
valentineonkui <90698966+valentineonkui@users.noreply.github.com>
2021-12-02 20:41:56 +0300

    Add files via upload

---
+++ project 2,L.c
@@ -0,0 +1,26 @@
+/*
+    project: county library milestone 1
+    author: valentine
+    date: 2nd December 2021
+    compiler: GCC
+    language: C99
+    license:MIT
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("school library\n");
+    printf("WELCOME Valentine\n");
+    printf("what would you like to do\n");
+    printf("1. add a new book\n");
+    printf("2. borrow another book");
+    printf("selected action: ");
+
+    return 0;
+
+}
